da230f086f939b1e1423c6d9e5839b5169bf7ed9
Nicole Song <xiuyuan128@yahoo.com>
2023-11-20 17:38:30 -0500

    111

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-import express from 'express';
-// const express = require('express')
-import Hello from "./hello.js"
+// const express = require("express");
+import express from "express";
+import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
+import CourseRoutes from "./courses/routes.js";
+import cors from "cors";
+import ModuleRoutes from "./modules/routes.js";
+import "dotenv/config";
 
-const app = express()
-Lab5(app)
-Hello(app)
-app.listen(4000)
+const app = express();
+
+app.use(cors());
+CourseRoutes(app);
+app.use(express.json());
+ModuleRoutes(app);
+
+Lab5(app);
+Hello(app);
+
+app.listen(process.env.PORT || 4000);
