29a79d17678fb485e927aaed983edc73b588c69a
Nicole Song <xiuyuan128@yahoo.com>
2023-12-04 18:02:07 -0500

    new data source

--- app.js
+++ app.js
@@ -15,13 +15,6 @@ const app = express();
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
 mongoose.connect(CONNECTION_STRING);
 
-
-/*app.use(
-    cors({      credentials: true,
-        origin: 'xxxxxx'}
-
-    )
-    );*/
    
 CourseRoutes(app);
 
