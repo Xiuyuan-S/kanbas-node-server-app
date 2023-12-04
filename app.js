269d6a7df5327f2efe215710f7b6953344da55c4
Nicole Song <xiuyuan128@yahoo.com>
2023-12-04 01:56:26 -0500

    a6

--- app.js
+++ app.js
@@ -1,20 +1,47 @@
 // const express = require("express");
-// const express = require("express");
 import express from "express";
 import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import cors from "cors";
-import mongoose from "mongoose";
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 import ModuleRoutes from "./modules/routes.js";
+import "dotenv/config";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
 import "dotenv/config";
 
 const app = express();
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+mongoose.connect(CONNECTION_STRING);
 
-app.use(cors());
+
+app.use(
+    cors({      credentials: true,
+        origin: 'xxxxxx'}
+
+    )
+    );
+   
 CourseRoutes(app);
+
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+app.use(session(sessionOptions));
+
+
 app.use(express.json());
+UserRoutes(app);
 ModuleRoutes(app);
 
 Lab5(app);
